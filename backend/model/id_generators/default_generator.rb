72f8adc0dfa7321ded5d3a78d9389a3e78972b82
jambun <jambun@gmail.com>
2016-07-14 13:03:24 +1000

    a safer sequence name

--- backend/model/id_generators/default_generator.rb
+++ backend/model/id_generators/default_generator.rb
@@ -1,7 +1,7 @@
 class DefaultGenerator < GeneratorInterface
 
   def generate(record)
-    Sequence.get("/repositories/#{record.repo_id}/archival_objects/component_id")
+    Sequence.get("/repositories/#{record.repo_id}/archival_objects/component_id_for_work_order")
   end
 
 end
