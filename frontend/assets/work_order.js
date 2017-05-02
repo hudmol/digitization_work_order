12a6a2b5229cf6f073b797d9954c71e4e65501b5
Mark Triggs <mark@dishevelled.net>
2017-05-02 11:26:05 +1000

    Show the original button in ArchivesSpace 1.5.x

--- frontend/assets/work_order.js
+++ frontend/assets/work_order.js
@@ -6,8 +6,18 @@ var WorkOrderToolbarAction = function() {
 WorkOrderToolbarAction.prototype.setupButton = function() {
    $workOrder = $(AS.renderTemplate("workOrderButtonTemplate"));
 
-  $('#other-dropdown .dropdown-menu').append($('<li />').append($workOrder));
-  $('#other-dropdown').show();
+  if ($('#other-dropdown .dropdown-menu').length > 0) {
+    /* ArchivesSpace 2.x */
+    $('#other-dropdown .dropdown-menu').append($('<li />').append($workOrder));
+    $('#other-dropdown').show();
+  } else {
+    /* ArchivesSpace 1.5.x */
+    $workOrder.addClass("btn btn-sm btn-default");
+    var $btnGroup = $("<div>").addClass("btn-group");
+
+    $workOrder.appendTo($btnGroup);
+    $btnGroup.appendTo($("#archives_tree_toolbar .btn-toolbar"));
+  }
 };
 
 
