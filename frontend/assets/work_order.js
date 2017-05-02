36c54896166296dc62aafa28e946da2d1b1eb790
Mark Triggs <mark@dishevelled.net>
2017-05-02 12:28:35 +1000

    Refactor JS to support 1.5.x and 2.0

--- frontend/assets/work_order.js
+++ frontend/assets/work_order.js
@@ -1,29 +1,33 @@
-var WorkOrderToolbarAction = function() {
-  this.setupButton();
+/* The past.  Eventually this (and the conditional below) can go away. */
+var setupButtonASpace15 = function() {
+  $workOrder = $(AS.renderTemplate("workOrderButtonTemplate"));
+
+  /* ArchivesSpace 1.5.x */
+  $workOrder.addClass("btn btn-sm btn-default");
+  var $btnGroup = $("<div>").addClass("btn-group");
+
+  $workOrder.appendTo($btnGroup);
+  $btnGroup.appendTo($("#archives_tree_toolbar .btn-toolbar"));
 };
 
-
-WorkOrderToolbarAction.prototype.setupButton = function() {
-   $workOrder = $(AS.renderTemplate("workOrderButtonTemplate"));
-
-  if ($('#other-dropdown .dropdown-menu').length > 0) {
-    /* ArchivesSpace 2.x */
-    $('#other-dropdown .dropdown-menu').append($('<li />').append($workOrder));
-    $('#other-dropdown').show();
-  } else {
-    /* ArchivesSpace 1.5.x */
-    $workOrder.addClass("btn btn-sm btn-default");
-    var $btnGroup = $("<div>").addClass("btn-group");
-
-    $workOrder.appendTo($btnGroup);
-    $btnGroup.appendTo($("#archives_tree_toolbar .btn-toolbar"));
-  }
+/* The future! */
+var setupButtonASpace2x = function() {
+  $workOrder = $(AS.renderTemplate("workOrderButtonTemplate"));
+  /* ArchivesSpace 2.x */
+  $('#other-dropdown .dropdown-menu').append($('<li />').append($workOrder));
+  $('#other-dropdown').show();
 };
 
 
 // setup the work order toolbar action
 $(document).on("loadedrecordform.aspace", function(event, $container) {
-  if (tree && tree.current().data('jsonmodel_type') == 'resource') {
-    new WorkOrderToolbarAction();
+  if (typeof(tree) === 'undefined') {
+    if (window.location.pathname.indexOf("/resources/") >= 0) {
+      setupButtonASpace15();
+    }
+  } else {
+    if (tree && tree.current().data('jsonmodel_type') == 'resource') {
+      setupButtonASpace2x();
+    }
   }
 });
