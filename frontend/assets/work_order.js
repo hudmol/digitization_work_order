2e095e4f572969766ad3fd7480ea1b356de92db8
Payten Giles <payten.giles@gmail.com>
2017-05-02 10:12:53 +1000

    Update js to show the work order toolbar action in the resource read only view's toolbar

--- frontend/assets/work_order.js
+++ frontend/assets/work_order.js
@@ -7,20 +7,13 @@ WorkOrderToolbarAction.prototype.setupButton = function() {
   this.$button = $(AS.renderTemplate("workOrderButtonTemplate"));
 
   var $btnGroup = $("<div>").addClass("btn-group");
+  $btnGroup.append(this.$button);
 
-  this.$button.appendTo($btnGroup);
-  $btnGroup.appendTo($("#archives_tree_toolbar .btn-toolbar"));
-}
+  $(".record-toolbar .btn-toolbar.pull-right > .btn-group:first > .btn-group:first").before($btnGroup);
+};
 
 
-// if a resource tree page, setup the work order toolbar action
+// setup the work order toolbar action
 $(document).on("loadedrecordform.aspace", function(event, $container) {
-  // are we dealing with a record with a tree?
-  if ($(".archives-tree").data("read-only") || $container.is("#archives_tree_toolbar")) {
-    // is the tree a resource tree?
-    if (AS._tree && AS._tree.get_json()[0].type == "resource") {
-      // hurray! add the button.
-      new WorkOrderToolbarAction();
-    }
-  }
+  new WorkOrderToolbarAction();
 });
